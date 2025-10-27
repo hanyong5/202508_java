6ee1657856c4cdc81ff2eabfa37a2775eba9ffc5
hanyong5 <hanyong5@naver.com>
2025-10-27 18:07:18 +0900

    mybatis

---
+++ 03spring/05_Mybatis_bbs/src/main/java/com/study/spring/bbs/IBbsDao.java
@@ -0,0 +1,14 @@
+package com.study.spring.bbs;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface IBbsDao {
+	public List<BbsDto> listDao();
+	public int countDao();
+	public BbsDto viewDao(String id);
+	
+}
